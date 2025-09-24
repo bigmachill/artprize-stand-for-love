c3ee91b3146f57570f6ad2751f0507942bd003b8
bigmachill <mackenziehill999@gmail.com>
2025-09-24 01:57:01 -0400

    Update test_printer.py

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,1 +1,9 @@
+from escpos.printer import Network
+
+# replace with your printer's IP address
+p = Network("192.168.1.150")  
+
+p.text("Hello from Raspberry Pi!\n")
+p.text("❤️ Stand for Love, Sit for Treats ❤️\n")
+p.cut()
 
